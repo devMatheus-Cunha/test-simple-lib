e6f7d3b8f4d2f9919873a76113600ed6fe5890ce
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-05-30 10:13:21 -0300

    feat: first test using jest

---
+++ src/lib/calculator.spec.js
@@ -0,0 +1,8 @@
+const {sum} = require("./calculator")
+
+it("should sum 2 and 2 the result must be 4", () => {
+  //! validation if the test is working
+  // expect(sum(2, 2)).not.toBe(4)
+
+   expect(sum(2, 2)).toBe(4)
+});
