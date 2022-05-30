cbc4859722e08657c18744236375f1adce7c5cd0
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-05-30 10:47:34 -0300

    feat: test if the values passed are string the result should be 4

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -6,3 +6,7 @@ it("should sum 2 and 2 the result must be 4", () => {
 
    expect(sum(2, 2)).toBe(4)
 });
+
+it('should sum 2 and 2 even if one the them is a string and the result must be 4 ', () => {
+  expect(sum("2", "2")).toBe(4)
+});
